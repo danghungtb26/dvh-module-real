70187ab6b9c5f3836ad736ff3ec77ab4d2b9efd0
Đặng Hùng <danghungtb26@gmail.com>
2021-01-14 11:03:31 +0700

    chore: test

--- packages/native-common/src/index.tsx
+++ packages/native-common/src/index.tsx
@@ -2,6 +2,7 @@ import { NativeModules } from 'react-native'
 
 type NativeCommonsType = {
   multiply(a: number, b: number): Promise<number>
+  add(): any
 }
 
 const { NativeCommons } = NativeModules
--- packages/splash/src/index.tsx
+++ packages/splash/src/index.tsx
@@ -3,6 +3,7 @@ import { NativeModules } from 'react-native'
 type SplashType = {
   show: (duration?: number) => void
   hide: (duration?: number) => void
+  none(): any
 }
 
 const { Splash } = NativeModules
