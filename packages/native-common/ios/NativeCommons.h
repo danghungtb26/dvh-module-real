8750b0e7e1e150a1b94120cd1952f0b828e4fb0c
Đặng Hùng <danghungtb26@gmail.com>
2021-01-14 09:20:49 +0700

    any

--- packages/native-common/ios/NativeCommons.h
+++
@@ -1,5 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface NativeCommons : NSObject <RCTBridgeModule>
-
-@end
--- packages/native-common/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type NativeCommonsType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { NativeCommons } = NativeModules;
-
-export default NativeCommons as NativeCommonsType;
